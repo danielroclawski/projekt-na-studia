5257ffce0d75ca857049644dbc705797f6f3c1fb
danielroclawski <93324574+danielroclawski@users.noreply.github.com>
2021-11-11 21:27:20 +0100

    ewr

--- program.py
+++ program.py
@@ -5,7 +5,7 @@ C = Canvas(top, bg="blue", height=1250, width=1300)
 filename = PhotoImage(file = "C:\\Users\\Daniel\\Downloads\\asd.png")
 background_label = Label(top, image = filename)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
-
+#dfsdg
 C.pack()
 C.mainloop()
 
