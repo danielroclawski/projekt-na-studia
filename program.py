1ba46cb12fadd3c3d7bbeb1ff0546d92744a0fd8
danielroclawski <93324574+danielroclawski@users.noreply.github.com>
2021-11-11 21:15:07 +0100

    dodanie zdjęcia

--- program.py
+++ program.py
@@ -1,3 +1,10 @@
-print("czołem")
-print("kluski z rosołem")
-print("cipa")
+from tkinter import *
+
+top = Tk()
+C = Canvas(top, bg="blue", height=1250, width=1300)
+filename = PhotoImage(file = "C:\\Users\\Daniel\\Downloads\\asd.png")
+background_label = Label(top, image = filename)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+C.pack()
+C.mainloop()
