4ba9e61a72eb005093ad4090c0f08b50d1251980
danielroclawski <93324574+danielroclawski@users.noreply.github.com>
2021-11-11 20:44:56 +0100

    Update program.py

--- program.py
+++ program.py
@@ -1,2 +1,12 @@
-print("czołem")
-print("kluski z rosołem")
+from tkinter import *
+
+top = Tk()
+C = Canvas(top, bg="blue", height=1250, width=1300)
+filename = PhotoImage(file = "C:\\Users\\Daniel\\Downloads\\asd.png")
+background_label = Label(top, image = filename)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+C.pack()
+C.mainloop()
+
+
