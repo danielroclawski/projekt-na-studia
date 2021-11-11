2cdbefb95117ccfb25e93000fd17dcadae4f999d
danielroclawski <93324574+danielroclawski@users.noreply.github.com>
2021-11-11 21:42:19 +0100

    ccvcv

--- program.py
+++ program.py
@@ -5,8 +5,6 @@ C = Canvas(top, bg="blue", height=1250, width=1300)
 filename = PhotoImage(file = "C:\\Users\\Daniel\\Downloads\\asd.png")
 background_label = Label(top, image = filename)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
-
+print("lodzik")
 C.pack()
 C.mainloop()
-
-
