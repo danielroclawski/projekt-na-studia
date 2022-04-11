30f66484150a0710dfda6fd2a7a832e60e7e5bdb
danielroclawski <93324574+danielroclawski@users.noreply.github.com>
2022-04-11 08:03:52 +0200

    Update program.py

--- program.py
+++ program.py
@@ -1,14 +1,4 @@
-#from tkinter import *
 
-#top = Tk()
-#C = Canvas(top, bg="blue", height=1250, width=1300)
-#filename = PhotoImage(file = "C:\\Users\\Daniel\\Downloads\\asd.png")
-#background_label = Label(top, image = filename)
-#background_label.place(x=0, y=0, relwidth=1, relheight=1)
-
-#C.pack()
-#C.mainloop()
-#nazwa programu
 
 
 #import biblioteki do wysyłania maili
